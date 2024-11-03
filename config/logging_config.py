69d527acc85ce030c51de3a5b81b35b3324a685f
Grisha <grisha765@tuta.io>
2024-11-04 02:32:12 +0300

    [u] update logging, use log file

--- config/logging_config.py
+++ config/logging_config.py
@@ -1,8 +1,6 @@
-import logging
-import os
+import logging, os
+from config.config import Config
 
-from config.config import Config 
-# Определяем ANSI escape-коды для цветов
 RESET = "\x1b[0m"
 WHITE = "\x1b[0m"
 COLORS = {
@@ -17,17 +15,30 @@ class ColoredFormatter(logging.Formatter):
     def format(self, record):
         log_color = COLORS.get(record.levelname, RESET)
         message = super().format(record)
-        # Извлечение имени файла без подчеркиваний
         filename = os.path.splitext(os.path.basename(record.pathname))[0]
         return f"{log_color}{record.levelname}{RESET}{WHITE}: {filename}: {message}{RESET}"
 
-def setup_logging(name='my_app'):
+class FileFormatter(logging.Formatter):
+    def format(self, record):
+        record.asctime = self.formatTime(record, self.datefmt)
+        filename = os.path.basename(record.pathname)
+        message = super().format(record)
+        return f"{record.asctime} - {record.levelname}: {filename}:{record.lineno} {message}"
+
+def setup_logging(name='my_app', log_file='/tmp/trans_tkpy.log'):
     logger = logging.getLogger(name)
-    if not logger.handlers:  # Проверяем, есть ли уже обработчики у логгера
-        handler = logging.StreamHandler()
-        formatter = ColoredFormatter('%(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(getattr(logging, Config.log_level, logging.INFO))  # Устанавливаем уровень логирования явно
+    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        stream_handler = logging.StreamHandler()
+        stream_formatter = ColoredFormatter('%(message)s')
+        stream_handler.setFormatter(stream_formatter)
+        stream_handler.setLevel(getattr(logging, Config.log_level, logging.INFO))
+        file_handler = logging.FileHandler(log_file)
+        file_formatter = FileFormatter('%(message)s')
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(getattr(logging, "DEBUG", logging.INFO))
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+    
     return logger
 
