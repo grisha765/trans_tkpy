355c346fb0126e96fc69efc0902ef7be69de2e4d
Grisha <grisha765@tuta.io>
2024-11-03 18:32:04 +0300

    [-] delete trash logs

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 logging.info(f"Script initialization, logging level: {Config.log_level}")
-logging.debug(f"coords: {Config.coords_var()}")
-logging.debug(f"lang: {Config.trans_lang}")
 
 if __name__ == '__main__':
     from core.window import window
