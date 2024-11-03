2c82f5e97517837478ba56646d77fed05afa4004
Grisha <grisha765@tuta.io>
2024-11-03 18:31:42 +0300

    [r] rewritten in lingva

--- trans/base.py
+++ trans/base.py
@@ -1,15 +1,40 @@
-from deep_translator import GoogleTranslator
+import urllib.parse
+import requests
 from config.config import Config
-import sys
 from core.clipboard import get_clipboard_text, get_clipboard_primary_text
+from config import logging_config
+logging = logging_config.setup_logging(__name__)
 
 def translate_text(text):
-    translated_text = GoogleTranslator(source='auto', target=Config.trans_lang).translate(text)
-    return translated_text
+    in_lang = Config.in_trans_lang
+    out_lang = Config.out_trans_lang
+    logging.debug(f"In trans lang: {in_lang}")
+    logging.debug(f"Out trans lang: {out_lang}")
+    logging.debug(f"In text: {text}")
+    text = urllib.parse.quote(text)
+    try:
+        response = requests.get(f"{Config.lingva_url}/api/v1/{in_lang}/{out_lang}/{text}", timeout=Config.request_timeout)
+        if response.status_code == 200:
+            data = response.json().get("translation", "Translate not found")
+            logging.debug(f"Out text: {data}")
+            return data
+        else:
+            msg = f"Error request: {response.status_code}"
+            logging.error(msg)
+            return msg
+    except requests.Timeout:
+        msg = f"Error request: Request timed out"
+        logging.error(msg)
+        return msg
+    except requests.RequestException as e:
+        msg = f"Error request: {e}"
+        logging.error(msg)
+        return msg
 
 def update_translation(root, messagebox, subprocess, use_primary, translation_label):
     text = get_clipboard_primary_text(messagebox, subprocess) if use_primary["use"] else get_clipboard_text(messagebox, subprocess)
     if not text:
+        import sys
         messagebox.showwarning("Warning", "No text found in clipboard.")
         root.destroy()
         sys.exit()
